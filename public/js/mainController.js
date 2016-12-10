2acf6f8eee313f7fdb39ee6bc57e4eb8635c86cd
Kabeer Rifaye <citizen.kabeer@gmail.com>
2016-12-11 00:19:23 +0530

    live URL

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -25,8 +25,17 @@ ngElastic.config(['$routeProvider', '$locationProvider', function($routeProvider
 
 ngElastic.controller('mainController', function($scope, $http) {
 	$scope.findIP = function() {
-		$http.get('/api/id').success(function(ids) {
-			$scope.routes = ids.hits.hits;
+		// $http.get('/api/id').success(function(ids) {
+		// 	$scope.routes = ids.hits.hits;
+		// }).error(function(err) {
+		// 	console.log(err.message);
+		// });
+		var url = 'http://10.12.21.14:9200/mpls_lsps/_search?search_type=count&pretty&source={%22aggregations%22:{%22states%22:{%22terms%22:{%22field%22:%22router_name%22},%22aggs%22:{%22hits%22:{%22top_hits%22:{%22size%22:1}}}}}}';
+		$http.get(url).success(function(data) {
+			var extractData =  data.aggregations.states.buckets.map(function(data) {
+				$scope.total = data.doc_count;
+				$scope.routes = data.hits.hits.hits;
+			});
 		}).error(function(err) {
 			console.log(err.message);
 		});
@@ -34,30 +43,57 @@ ngElastic.controller('mainController', function($scope, $http) {
 });
 
 ngElastic.controller('ipController', function($scope, $http, $routeParams) {
-	$http.get('/api/id/'+ $routeParams.id).success(function(data) {
-		$scope.data = data;
-		for (var i in data._source.existing_config){
+	// $http.get('/api/id/'+ $routeParams.id).success(function(data) {
+	// 	$scope.data = data;
+	// 	for (var i in data._source.existing_config){
+	//     	$scope.router_name = i;
+	//     	$scope.existing_config = data._source.existing_config[i];
+	// 	}
+	// 	for (var i in data._source.new_config){
+	//     	$scope.new_config = data._source.new_config[i];
+	// 	}
+	// }).error(function(err) {
+	// 	console.log(err.message);
+	// });
+	$http.get('http://10.12.21.14:9200/mpls_lsps/_search?q=_id:'+$routeParams.id+'&pretty=true').success(function(data) {
+		var dataById = data.hits.hits[0];
+		$scope.data = data.hits.hits[0];
+		for (var i in dataById._source.existing_config){
 	    	$scope.router_name = i;
-	    	$scope.existing_config = data._source.existing_config[i];
+	    	$scope.existing_config = dataById._source.existing_config[i];
 		}
-		for (var i in data._source.new_config){
-	    	$scope.new_config = data._source.new_config[i];
+		for (var i in dataById._source.new_config){
+	    	$scope.new_config = dataById._source.new_config[i];
 		}
-	}).error(function(err) {
+
+     	}).error(function(err) {
 		console.log(err.message);
 	});
+	// $scope.postData = function(req, res) {
+	// 	var routerInfo = {
+	// 		Router_name: $scope.data._source.router_name,
+	// 		LSP_name: $scope.data._id,
+	// 		Hops: $scope.existing_config,
+	// 		NodeSeq: $scope.new_config
+	// 	};
+	// 	var Result = {config: routerInfo};
+	// 	$http.post('/api/apply', Result).success(function(response) {
+	// 		console.log("Posted");
+	// 	}).error(function(res) {
+	// 		console.log(res.message);
+	// 	});
+	// };
 	$scope.postData = function(req, res) {
-		var routerInfo = {
+        var routerInfo = {
 			Router_name: $scope.data._source.router_name,
 			LSP_name: $scope.data._id,
 			Hops: $scope.existing_config,
 			NodeSeq: $scope.new_config
 		};
-		var Result = {config: routerInfo};
-		$http.post('/api/apply', Result).success(function(response) {
+		$http.post('http://10.12.21.14:9095/apply', routerInfo).success(function(response) {
 			console.log("Posted");
 		}).error(function(res) {
-			console.log(res.message);
+         	console.log(res.message);
 		});
 	};
 });
