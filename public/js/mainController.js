f86f2303de90f8d9cc1d8d258507f43bdd4a5451
Kabeer Rifaye <citizen.kabeer@gmail.com>
2016-12-17 22:09:39 +0530

    code clean

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -37,19 +37,11 @@ ngElastic.config(['$routeProvider', '$locationProvider', function($routeProvider
 
 ngElastic.controller('mainController', function($scope, $http) {
 	$scope.findIP = function() {
-		// $http.get('/api/id').success(function(ids) {
-		// 	$scope.routes = ids.hits.hits;
-		// }).error(function(err) {
-		// 	console.log(err.message);
-		// });
 		// var url = 'http://10.12.21.14:9200/mpls_lsps/_search?search_type=count&pretty&source={%22aggregations%22:{%22states%22:{%22terms%22:{%22field%22:%22router_name%22},%22aggs%22:{%22hits%22:{%22top_hits%22:{%22size%22:1}}}}}}';
 		$http.get('/api/routes').success(function(data) {
 			$scope.routes = data.aggregations.states.buckets;
 			var extractData =  data.aggregations.states.buckets.map(function(data) {
-				// console.log(data);
 				$scope.total = data.doc_count;
-				// $scope.routes = data;
-				// console.log($scope.routes);
 			});
 		}).error(function(err) {
 			console.log(err.message);
@@ -58,18 +50,6 @@ ngElastic.controller('mainController', function($scope, $http) {
 });
 
 ngElastic.controller('ipController', function($scope, $http, $routeParams) {
-	// $http.get('/api/id/'+ $routeParams.id).success(function(data) {
-	// 	$scope.data = data;
-	// 	for (var i in data._source.existing_config){
-	//     	$scope.router_name = i;
-	//     	$scope.existing_config = data._source.existing_config[i];
-	// 	}
-	// 	for (var i in data._source.new_config){
-	//     	$scope.new_config = data._source.new_config[i];
-	// 	}
-	// }).error(function(err) {
-	// 	console.log(err.message);
-	// });
 	// var url = 'http://10.12.21.14:9200/mpls_lsps/_search?q=_id:'+$routeParams.id+'&pretty=true';
 	$scope.init = function() {
 		$http.get('/api/route/'+$routeParams.routername).success(function(data) {
@@ -78,55 +58,26 @@ ngElastic.controller('ipController', function($scope, $http, $routeParams) {
 			console.log(err.message);
 		});
 	}
-	// $scope.postData = function(req, res) {
-	// 	var routerInfo = {
-	// 		Router_name: $scope.data._source.router_name,
-	// 		LSP_name: $scope.data._id,
-	// 		Hops: $scope.existing_config,
-	// 		NodeSeq: $scope.new_config
-	// 	};
-	// 	var Result = {config: routerInfo};
-	// 	$http.post('/api/apply', Result).success(function(response) {
-	// 		console.log("Posted");
-	// 	}).error(function(res) {
-	// 		console.log(res.message);
-	// 	});
-	// };
 	$scope.postData = function(i, d) {
-		// var routerInfo = {
-		// 	Router_name: $scope.data._source.router_name,
-		// 	LSP_name: $scope.data._id,
-		// 	Hops: $scope.existing_config,
-		// 	NodeSeq: $scope.new_config
-		// };
-		// console.log(d);
-		// $scope.data.forEach(function(d) {
-	        $scope.routerInfo = {
-				Router_name: d._source.router_name,
-				LSP_name: d._id,
-				Hops: d._source.existing_config,
-				NodeSeq: d._source.new_config
-			};
-			$scope.anotherRouteInfo = {
-				router_name: d._source.router_name,
-				router_address: d._source.router_address,
-				new_config: d._source.new_config
-			};
-		// });
-		console.log($scope.routerInfo);
-		console.log($scope.anotherRouteInfo);
+        $scope.routerInfo = {
+			Router_name: d._source.router_name,
+			LSP_name: d._id,
+			Hops: d._source.existing_config,
+			NodeSeq: d._source.new_config
+		};
+		$scope.anotherRouteInfo = {
+			router_name: d._source.router_name,
+			router_address: d._source.router_address,
+			new_config: d._source.new_config
+		};
 		$http.post('/api/applyanother', $scope.anotherRouteInfo).success(function(res) {
-			console.log("posting another Router");
 			toastr.error("Posted Success");
 		}).error(function(res) {
-			console.log(res.message);
 			toastr.info('Internal server error in another Router');
 		});
 		$http.post('/api/apply', $scope.routerInfo).success(function(response) {
-			console.log("Posted");
 			toastr.success('Posted Successfully');
 		}).error(function(res) {
-			console.log(res.message);
          	toastr.error('Internal Server Error');
 		});
 		$scope.data.splice(i, 1);
@@ -174,6 +125,7 @@ ngElastic.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, r
   };
 });
 
+// notFound Controller
 ngElastic.controller('notFound', function($scope) {
 	$scope.title = "Not FOUND";
 });
