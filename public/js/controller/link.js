1298795db5b608ca13c02ce4a7a3add30ecf80ae
Kabeer Rifaye <citizen.kabeer@gmail.com>
2017-02-09 03:45:23 +0530

    replace randomColor to given input - Link

--- public/js/controller/link.js
+++ public/js/controller/link.js
@@ -4,40 +4,26 @@ ngElastic.controller('linkController', function($scope, $http) {
 
 	// Load initially when the link page called.
 	$scope.initLink = function() {
-		// $scope.isLoading = true;
 		$http.get('/api/link').success(function(d) {
 			$scope.dataset = d.hits.hits;
 			var bw = _.map(d.hits.hits, function(d) {
 				$scope.valueLength = d._source.b_w.length/2;
 			});
 		});
-		// $scope.isLoading = false;
 	};
 
 	// Get the b_w values in new line each
 	$scope.getDescValue = function(v) {
 		$scope.isLoading = true;
+		var values = [],colorCode = [];
 		var descriptionValue = _.filter(v, function(d) {
-			return d.indexOf("#") !== 0;
+				if(d.indexOf("#") !== 0)
+					values.push(d);
+				else
+					colorCode.push(d)
 		});
-		return descriptionValue;
+		$scope.colors = colorCode;
+		return values;
 		$scope.isLoading = false;
-	}
-
-	// Get the color code to set as bg color for td.
-	// Set the array[0] as bg color.
-	// $scope.getBGColor = function(v) {
-	// 	var descriptionBGColor = _.filter(v, function(d) {
-	// 		return d.indexOf("#") == 0;
-	// 	});
-	// 	return descriptionBGColor;
-	// }
-	$scope.getRandomColor = function() {
-		var hexCode = '0123456789ABCDEF'.split(''),
-        colorCode = '#';
-    for(var i = 0; i < 6; i++) {
-        colorCode += hexCode[Math.round(Math.random() * 15)];
-    }
-    return {'background-color': colorCode};
 	}
 });
