166bd437493391fb1eeb5234c92cd1a972efc36b
Kabeer Rifaye <citizen.kabeer@gmail.com>
2017-02-16 23:59:52 +0530

    load data using limit - lsp mesh

--- public/js/controller/lspmesh.js
+++ public/js/controller/lspmesh.js
@@ -1,9 +1,13 @@
 ngElastic.controller('lspMeshController', function($scope, $http) {
 	// Title
 	$scope.lspMesh = "LSP Mesh";
-
+	$scope.defaultLimit = 10;
+	$scope.limit = 10;
+	$scope.more = "(...More)";
+	$scope.less = "(Less)";
 	// Load onload
 	$scope.initLspMesh = function() {
+		// $http.get('/proxy/lsp_sum/stats/_search?size=1000&pretty=true&query=matchAll').success(function(d) {
 		$http.get('/api/lspmesh').success(function(lsps) {
 			$scope.lsps = lsps.hits.hits;
 		}).error(function(err) {
@@ -11,4 +15,8 @@ ngElastic.controller('lspMeshController', function($scope, $http) {
 		})
 	};
 
+	// Missing Routers
+	$scope.limitMissRoute = function(route) {
+		return route;
+	};
 });
