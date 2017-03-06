3bf1d8d28f30d47181d71aacd7180b883b0c7eef
Kabeer Rifaye <citizen.kabeer@gmail.com>
2017-03-06 20:59:36 +0530

    reworked table

--- public/js/controller/table.js
+++ public/js/controller/table.js
@@ -25,3 +25,167 @@ ngElastic.controller('tableController', function($scope, $http) {
 		$scope.isLoading = false;
 	};
 });
+
+/*  Mar 06 */
+
+// cdnDashboard.controller('ServerHealthController', function($scope, es) {
+//   es.cluster.health(function (err, resp) {
+//     if (err) {
+//     	$scope.data = err.message;
+//     } else {
+//       $scope.data = resp;
+//     }
+//   });
+// });
+// cdnDashboard.service('es', function (esFactory) {
+//   return esFactory({ host:  'http://10.12.22.10:9200/'
+//  });
+// });
+// cdnDashboard.controller('tableController', function($scope, $http,$timeout,es) {
+// 	$scope.isCollapsed = true;
+// 	$scope.initTableController = function() {
+// 		$http.get('/proxy/gw_info/stats/_search?pretty&size=100&sort=latest_speed:desc').success(function(d){
+//    		$scope.dataset = d.hits.hits;
+//    		_.remove($scope.dataset, {_source:{interface: 'ae400'}});
+//    		// var res = [];   		
+//    		// console.log(d.hits.hits._source.partner,d.hits.hits._source.AS,d.hits.hits._source.partner_type,d.hits.hits._source.speed,d.hits.hits._source.direction);
+//       $scope.dataset.forEach(function(d){
+//    	  	var deviceName = d._source.device.replace(/-/g,'_');
+//   	   	var interface = d._source.interface;
+//   	   	var label1 = [];
+//   	   	var statSource = d._source;
+//   	   	//Elatic query
+//       	es.search({
+// 					index: 'desc_map',
+// 					type: 'config',
+// 					size: 10000,
+// 					body: {
+// 						"query": 
+// 					  	{"bool": 
+// 					     	{"must": 
+// 				         	[
+// 				         		{"match":
+// 			             		{"device":deviceName}
+// 			            	},
+// 		              	{"match":
+//                   		{"intf": interface}
+// 		                }
+//                  	]
+// 	             	}
+// 		         	}
+// 			    }
+// 				}).then(function (response) {
+// 					// console.log("Response", response);
+// 					if(response.hits.hits.length > 0) {
+// 						_.map(response.hits.hits, function(d) {
+// 							var label = "mpls"+d._source.label,
+// 								device = d._source.device,
+// 								intf = d._source.intf;
+// 							compareLabel(label,device,intf,statSource);
+// 						});
+// 					} else {
+// 							// label1.push("No Hits Found");
+// 							console.log("No Labels Found");
+// 					}
+					
+// 				});
+// 				// console.log("No Label1s : ",label1);
+//   		});
+//   		function compareLabel(l, dev, i,cb) {
+//   			// console.log(l,dev,i,cb);
+//   			var fTime = [];
+//   			var fBps = [];
+//   			$scope.newObj = [];
+// 	  		$http.get('/proxy/testing52/cdnstats/_search?size=100&q=hostip:all&sort=time:desc').success(function(d){
+// 					var dHits = d.hits.hits;
+// 					// console.log(dHits.length);
+// 					$scope.fiilteredData = [];
+// 						_.map(dHits, function(_src) {
+// 							var time = moment(new Date(_src._source.time*1000));
+// 							var monthnday = time.format('MM/DD, H:mm');
+// 							_.map(_src._source.IntfRawTrend, function(name) {
+// 								if(name.intfname == l) {
+// 									fTime.push(monthnday);
+// 									fBps.push(name.bps);
+// 									// $scope.fiilteredData.push(
+// 									// 	{
+// 									// 		_source: {
+// 									// 			device: dev,
+// 									// 			interface: i,
+// 									// 			partner: "partner",
+// 									// 			AS: "AS",
+// 									// 			partner_type: "partner_type",
+// 									// 			speed: "speed",
+// 									// 			direction: "direction",
+// 									// 			intfname:name.intfname,
+// 									// 			bps:name.bps,
+// 									// 			b_w: [
+// 									// 				"1",
+// 									// 				"2",
+// 									// 				"3"
+// 									// 			]
+// 									// 		}
+// 									// 	}
+// 									// );
+// 								}
+// 							});
+// 						});
+// 						$scope.newObj.push({
+// 							_source: {
+// 								device: dev,
+// 								interface: i,
+// 								partner: cb.partner,
+// 								AS: cb.AS,
+// 								partner_type: cb.partner_type,
+// 								speed: cb.speed,
+// 								direction: cb.direction,
+// 								time: fTime,
+// 								b_w: fBps.toString().split(",")
+// 							}
+// 						});
+// 	    	});
+//   		}
+//      	$scope.totalItems = $scope.dataset.length;
+//      	_.map(d.hits.hits, function(d) {
+// 				$scope.valueLength = d._source.b_w.length/2;
+// 				$scope.heading = d._source.heading;
+// 				// console.log($scope.heading);
+// 			});
+// 			// check Readio Buttons
+// 			$scope.checkRadioButton = function(value) {
+// 				var datasetCopy = angular.copy($scope.dataset);
+// 				if(value == "default"){
+// 					console.log(value);
+// 					// $scope.value = value;
+// 					$scope.dataset = datasetCopy;
+// 				}else{
+// 					// console.log(value);
+// 					$scope.value = value;
+// 					$scope.filHeading = $scope.newObj[0]._source.time;
+// 					$scope.filDataset=$scope.newObj;
+// 				}
+// 			};
+// 			// set default value for radio button
+// 			$scope.radio = {
+// 				"value": "default"
+// 			}
+//     });
+// 	};
+
+
+// // Get the b_w values in new line each			   
+//  	$scope.getDescValue = function(v) {
+// 		$scope.isLoading = true;
+// 		var values = [],colorCode = [];
+// 		var descriptionValue = _.filter(v, function(d) {
+// 			if(d.indexOf("#") != 0)
+// 				values.push(d);
+// 			else
+// 				colorCode.push(d)
+// 		});
+
+// 		$scope.colors = colorCode;
+// 		return values;
+// 		$scope.isLoading = false;
+// 	}
+// });	
