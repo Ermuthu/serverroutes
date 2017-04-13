f850cb3ad49ec2a396c91bbeb2b747973dcedb3c
Kabeer Rifaye <citizen.kabeer@gmail.com>
2017-04-13 09:37:52 +0530

    updated map_link JSON and highlight black line separate - Map

--- public/js/controller/mapController.js
+++ public/js/controller/mapController.js
@@ -8,23 +8,26 @@ ngElastic.controller('mapController', function($scope, $http, $routeParams) {
 		var rp = $routeParams.sourcenames,
 			Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}},
 	  	decode = Base64.decode(rp);
+  	// console.log(rp);
 	  var arrayFromParams =  _.split(decode, ',');
-	  console.log(arrayFromParams);
+	  // console.log(arrayFromParams);
 	  var toStr = _.toString(arrayFromParams);
+	  // console.log(toStr);
 	  var removeUnderscore = _.replace(toStr, /[_-]/g, ",");
 	  $scope.node_coor= _.split(removeUnderscore, ',');
 	  console.log($scope.node_coor);
 	 }
 
 	// Re-arrange the params to array of object for src and dest
+	var five = 5;
 	var connLineData = [];
 	if($routeParams.sourcenames != undefined){
 		for(var i=0; i<$scope.node_coor.length-3; i += 2) {
 			connLineData.push({
-				src_x: $scope.node_coor[i],
-				src_y: $scope.node_coor[i+1],
-				dest_x: $scope.node_coor[i+2],
-				dest_y: $scope.node_coor[i+3]
+				src_x: parseInt($scope.node_coor[i]) + five,
+				src_y: parseInt($scope.node_coor[i+1]) + five,
+				dest_x: parseInt($scope.node_coor[i+2]) + five,
+				dest_y: parseInt($scope.node_coor[i+3]) + five
 			});
 		}
 	}
@@ -96,8 +99,8 @@ ngElastic.controller('mapController', function($scope, $http, $routeParams) {
 		return replaceSymbol(val.replace(/_/g, '-'));
 	};
 
-	$scope.hoverIn = function(sx,sy,dx,dy) {
-		console.log(sx,sy,dx,dy);
-	}
+	// $scope.hoverIn = function(sx,sy,dx,dy) {
+	// 	console.log(sx,sy,dx,dy);
+	// }
 
 });
