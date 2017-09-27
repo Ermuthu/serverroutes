ae95bee6e3abca7e3cef9ef6effd8027151b906e
Kabeer Rifaye <citizen.kabeer@gmail.com>
2017-09-27 11:13:44 +0530

    custom files

---
+++ public/js/custom/custom.js
@@ -0,0 +1,31 @@
+// $("#zoomSVG").anythingZoomer({
+//   smallArea: 'small',
+//   largeArea: 'large',
+//   clone: true,
+//   overlay: false,
+//   speed: 100,
+//   edge: 'auto',
+//   offsetX: 0,
+//   offsetY: 0,
+//   switchEvent: 'dblclick',
+//   delay: 0,
+//   edit: false,
+//   initialzied: function(e, zoomer) {},
+//   zoom: function(e, zoomer) {},
+//   unzoom: function(e, zoomer) {}
+// });
+
+// (function() {
+//   var $section = $('div').first();
+//   $section.find('.panzoom').panzoom({
+//     $zoomIn: $section.find(".zoom-in"),
+//     $zoomOut: $section.find(".zoom-out"),
+//     $zoomRange: $section.find(".zoom-range"),
+//     $reset: $section.find(".reset")
+//   });
+
+//   $('#myCarousel').carousel({
+//     pause: true,
+//     interval: false
+//   });
+// })();
